242aa629038ef0b9458fc9c088fcbc78f399bf09
Blake Strickland <Stricklandb02@gmail.com>
2015-10-24 14:49:53 -0400

    Fix questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,15 @@
 class QuestionsController < ApplicationController
   def create
+  question = Question.new(member_id: params[:member_id], title: params[:title], desc: params[:desc])
+    if question.member.token == params[:token]
+      if question.save
+        render json: question
+      else
+        render json: question.errors
+      end
+    else
+      render json: "You shall not pass"
+    end
   end
 
   def index
@@ -8,6 +18,32 @@ class QuestionsController < ApplicationController
 
   def show
     render json: Question.find(params[:id])
+  end
+
+  def update
+    question = Question.find(params[:id])
+    if question.member.token == params[:token]
+      if question.save
+        render json: question
+      else
+        render json: question.errors
+      end
+    else
+      render json: "You shall not pass"
+    end
+  end
+
+  def destroy
+    question = Question.find(params[:id])
+    if question.member.token == params[:token]
+      if question.destroy
+        render json: "ITS HAD BEEN DECIDED"
+      else
+        render json: question.errors
+      end
+    else
+      render json: "You shall not pass"
+    end
   end
   
   def as_json(arg)
